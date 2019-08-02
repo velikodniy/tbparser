3838afd0359ca52783c644219005af6b1635b5d4
Vadim Velicodnii <vadim@velikodniy.name>
2019-08-02 12:15:10 +0100

    Make version avaliable from __init__

--- tbparser/__init__.py
+++ tbparser/__init__.py
@@ -1,8 +1,10 @@
 from tbparser.events_reader import EventsFileReader, EventReadingError
 from tbparser.summary_reader import SummaryReader
+from tbparser.version import __version__
 
 __all__ = [
     'EventsFileReader',
     'EventReadingError',
     'SummaryReader',
+    '__version__',
 ]
